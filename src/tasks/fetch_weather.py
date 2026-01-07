268b53e35b44cfcbc8ba5e5bf74d9101a15c56b2
Solo Shun <88045720+soloshun@users.noreply.github.com>
2026-01-07 18:26:19 +0000

    fead: Add fetch_weather task and update requirements

---
+++ src/tasks/fetch_weather.py
@@ -0,0 +1,75 @@
+import openmeteo_requests
+
+import pandas as pd
+import requests_cache
+from retry_requests import retry
+
+from prefect import task
+
+LONDON_LAT = 51.50853
+LONDON_LON = -0.12574
+
+# @task
+def fetch_weather(lat: float=LONDON_LAT, lon: float=LONDON_LON) -> pd.DataFrame:
+    # Setup the Open-Meteo API client with cache and retry on error
+    cache_session = requests_cache.CachedSession('../../data/raw/.cache', expire_after = 3600)
+    retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+    openmeteo = openmeteo_requests.Client(session = retry_session)
+
+    # Make sure all required weather variables are listed here
+    # The order of variables in hourly or daily is important to assign them correctly below
+    url = "https://api.open-meteo.com/v1/forecast"
+    params = {
+        "latitude": lat,
+        "longitude": lon,
+        "hourly": ["precipitation", "wind_speed_10m", "wind_gusts_10m", "relative_humidity_2m", "temperature_2m", "surface_pressure"],
+        "timezone": "Europe/London",
+        "start_date": _get_yesterdays_date(),
+        "end_date": _get_yesterdays_date(),
+    }
+    responses = openmeteo.weather_api(url, params=params)
+
+    # Process first location. Add a for-loop for multiple locations or weather models
+    response = responses[0]
+    # print(f"Coordinates: {response.Latitude()}°N {response.Longitude()}°E")
+    # print(f"Elevation: {response.Elevation()} m asl")
+    # print(f"Timezone: {response.Timezone()}{response.TimezoneAbbreviation()}")
+    # print(f"Timezone difference to GMT+0: {response.UtcOffsetSeconds()}s")
+
+    # Process hourly data. The order of variables needs to be the same as requested.
+    hourly = response.Hourly()
+    hourly_precipitation = hourly.Variables(0).ValuesAsNumpy()
+    hourly_wind_speed_10m = hourly.Variables(1).ValuesAsNumpy()
+    hourly_wind_gusts_10m = hourly.Variables(2).ValuesAsNumpy()
+    hourly_relative_humidity_2m = hourly.Variables(3).ValuesAsNumpy()
+    hourly_temperature_2m = hourly.Variables(4).ValuesAsNumpy()
+    hourly_surface_pressure = hourly.Variables(5).ValuesAsNumpy()
+
+    hourly_data = {"date": pd.date_range(
+        start = pd.to_datetime(hourly.Time(), unit = "s", utc = True),
+        end =  pd.to_datetime(hourly.TimeEnd(), unit = "s", utc = True),
+        freq = pd.Timedelta(seconds = hourly.Interval()),
+        inclusive = "left"
+    )}
+
+    hourly_data["temperature_2m"] = hourly_temperature_2m
+    hourly_data["precipitation"] = hourly_precipitation
+    hourly_data["relative_humidity_2m"] = hourly_relative_humidity_2m
+    hourly_data["wind_speed_10m"] = hourly_wind_speed_10m
+    hourly_data["wind_gusts_10m"] = hourly_wind_gusts_10m
+    hourly_data["surface_pressure"] = hourly_surface_pressure
+
+    hourly_dataframe = pd.DataFrame(data = hourly_data)
+    # print("\nHourly data\n", hourly_dataframe)
+    return hourly_dataframe
+
+
+def _get_yesterdays_date() -> str:
+    from datetime import datetime, timedelta, timezone
+    yesterday = datetime.now(timezone.utc) - timedelta(days=1)
+    return yesterday.strftime("%Y-%m-%d")
+
+
+if __name__ == "__main__":
+    df = fetch_weather()
+    print(df)
